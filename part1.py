500d6143f6c49d4af7bdd139fe735f6ef39a393a
Yudi Tan <yudi98@berkeley.edu>
2020-10-29 10:25:39 -0500

    part 1 fully working

--- part1.py
+++ part1.py
@@ -3,15 +3,8 @@ import numpy as np
 
 # setting up video capture
 cam = cv2.VideoCapture(0)
-
-lower_HSV = np.array([0, 40, 0], dtype = "uint8")  
-upper_HSV = np.array([25, 255, 255], dtype = "uint8")  
-
-
-
-
-lower_YCrCb = np.array((0, 138, 67), dtype = "uint8")  
-upper_YCrCb = np.array((255, 173, 133), dtype = "uint8")  
+cam.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.25) # 0.25 turns OFF auto exp
+cam.set(cv2.CAP_PROP_AUTO_WB, 0.25) # 0.25 turns OFF auto WB
 
 
 
@@ -19,16 +12,16 @@ def update_(x):
     pass 
 
 while True:
+    lower_HSV = np.array([0, 48, 80], dtype = "uint8")  
+    upper_HSV = np.array([25, 255, 255], dtype = "uint8")  
+    lower_YCrCb = np.array((0, 138, 67), dtype = "uint8")  
+    upper_YCrCb = np.array((255, 173, 133), dtype = "uint8")  
     ret, frame = cam.read()
     if not ret:
         print("Unable to capture video")
         break
 
     # part 1 
-    cv2.namedWindow("part_1")    
-    # Create a Trackbar to choose a value for a parameter    
-    # cv2.createTrackbar(parameter_value_name, window_name , parameter_min_value, parameter_max_value, nothing)  
-
     convertedHSV = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  
     skinMaskHSV = cv2.inRange(convertedHSV, lower_HSV, upper_HSV)  
 
@@ -38,7 +31,7 @@ while True:
 
     skinMask = cv2.add(skinMaskHSV,skinMaskYCrCb)  
 
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (7, 7))  
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (11, 11))  
     skinMask = cv2.erode(skinMask, kernel, iterations = 2)  
     skinMask = cv2.dilate(skinMask, kernel, iterations = 2)  
     
@@ -46,7 +39,7 @@ while True:
     # mask to the frame  
     skinMask = cv2.GaussianBlur(skinMask, (3, 3), 0) 
     skin = cv2.bitwise_and(frame, frame, mask = skinMask) 
-    cv2.imshow('frame', skin)
+    cv2.imshow('frame', np.hstack([frame, skin]))
     k = cv2.waitKey(1)
 
 
